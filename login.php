8c88d90b526aa844f0a4bfc7d865e5096df30b9f
Cristina Morillo <cmoril01@ucm.es>
2023-03-02 12:58:57 +0100

    Login y Registro
    login.php y registro.php iniciales

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>La Quinta Caja Login</title>
+</head>
+
+<body>
+
+<div id="contenedor">
+
+	<?php
+	include("includes/comun/cabecera.php");
+	?>
+	<div id="contenido">
+		<form action="procesarLogin.php" method="POST">
+			<fieldset>
+			<legend>Usuario y contraseña</legend>
+				<p><label>Email:</label> <input type="text" name="email" /></p>
+				<p><label>Contraseña:</label> <input type="password" name="password" /></p>
+				<button type="submit">Entrar</button>
+				<p>No tengo cuenta <a href="registro.php">Regístrate aquí</a></p>
+			</fieldset>
+		</form>
+	</div>
+</div> 
+</body>
+</html>
---
+++ registro.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>La Quinta Caja Registro</title>
+</head>
+<body>
+<div id="contenedor">
+    <?php
+    include("includes/comun/cabecera.php");
+    ?>
+    <div id="contenido">
+    <form action="procesarRegistro.php" method="post">
+    <fieldset>
+        <legend>Registro Usuario</legend>
+        <div>
+            <p><label for="nombre">Nombre:</label>
+            <input type="nombre" id="nombre" name="nombre" required>
+        </div></p>
+        <div>
+            <p><label for="apellidos">Apellidos:</label>
+            <input type="apellidos" id="apellidos" name="apellidos" required></p>
+        </div>
+        <div>
+            <p><label for="email">Email:</label>
+            <input type="email" id="email" name="email" required></p>
+        </div>
+        <div>
+            <p><label for="password1">Contraseña:</label>
+            <input type="password" id="password1" name="password1" required></p>
+        </div>
+        <div>
+            <p><label for="password2">Confirmar contraseña:</label>
+            <input type="password" id="password2" name="password2" required></p>
+        </div>
+        <div>
+            <p><label for="direccion">Dirección:</label>
+            <input type="direccion" id="direccion" name="direccion" required></p>
+        </div>
+        <div>
+            <p><input type="checkbox" id="terms" name="terms" required>
+            <label for="terms">Acepto los términos y condiciones</label></p>
+        </div>
+        <div>
+            <button type="submit">Registrarse</button>
+        </div>
+    </fieldset>
+    </form>
+    </div>
+</div>
+</body>
+</html>
+</html>
