709846c3fb8336b37c30a295fa4bbcfb75446ecc
sergioati <sergioat@ucm.es>
2023-03-05 16:40:44 +0100

    Mostrar Producto

---
+++ producto.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+    <title>La Quinta Caja</title>
+    <link href="css/index.css" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8">
+  </head>
+  <body>
+	<?php
+		include("includes/comun/cabecera.php");
+	?>
+    <div class="logo-contenedor">
+      <img src="img/logo.png" alt="Logo tienda">
+    </div>
+    <h1>La Quinta Caja</h1>
+    <p>Hoy en día la contaminación provocada por el fast fashion está en aumento, naciendo así La Quinta Caja, una empresa sostenible. La Quinta Caja es un e-Commerce dedicado a la venta de cajas sorpresa (especialmente de ropa de segunda mano). Las cajas se pueden comprar individualmente o mediante suscripciones mensuales. Cada producto se puede comprar en diferentes tallas y tamaños. Los usuarios registrados que compren podrán expresar su opinión en las reviews. Otro servicio que se ofrece dentro de la página es un blog en el que los administradores pueden escribir noticias y consejos relacionados con la empresa, pudiendo los usuarios comentar. En la página principal se podrán encontrar los artículos de temporada. La tienda se podrá mostrar mediante categorías (cajas de ropa, cajas de temporadas, cajas especiales…) y se podrán aplicar filtros. Los productos serán añadidos y modificados por los administradores y estos podrán ver las compras realizadas.</p>
+  </body>
+</html>
