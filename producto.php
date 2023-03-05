1c0c5a609889530cef594d46f5b9e458b7788236
sergioati <sergioat@ucm.es>
2023-03-05 17:03:12 +0100

    Mostrar producto

--- producto.php
+++ producto.php
@@ -9,10 +9,34 @@
 	<?php
 		include("includes/comun/cabecera.php");
 	?>
-    <div class="logo-contenedor">
-      <img src="img/logo.png" alt="Logo tienda">
-    </div>
-    <h1>La Quinta Caja</h1>
-    <p>Hoy en día la contaminación provocada por el fast fashion está en aumento, naciendo así La Quinta Caja, una empresa sostenible. La Quinta Caja es un e-Commerce dedicado a la venta de cajas sorpresa (especialmente de ropa de segunda mano). Las cajas se pueden comprar individualmente o mediante suscripciones mensuales. Cada producto se puede comprar en diferentes tallas y tamaños. Los usuarios registrados que compren podrán expresar su opinión en las reviews. Otro servicio que se ofrece dentro de la página es un blog en el que los administradores pueden escribir noticias y consejos relacionados con la empresa, pudiendo los usuarios comentar. En la página principal se podrán encontrar los artículos de temporada. La tienda se podrá mostrar mediante categorías (cajas de ropa, cajas de temporadas, cajas especiales…) y se podrán aplicar filtros. Los productos serán añadidos y modificados por los administradores y estos podrán ver las compras realizadas.</p>
-  </body>
+  <div class="product">
+  <img src="producto_id.jpg" alt="Imagen del producto">
+  <div class="info_producto">
+    <h2>Nombre del producto</h2> //esto se sacará de la base de datos, mediante clase ProductoSA, que tendrá método obtener producto, que nos dará toda la información requerida
+    <p>Descripción del producto</p>
+    <form>
+      <label for="talla">Talla:</label>
+      <select id="talla" name="size">//las tallas también se sacarán de la base de datos
+        <option value="s">S</option>
+        <option value="m">M</option>
+        <option value="l">L</option>
+        <option value="xl">XL</option>
+      </select>
+      <label for="cantidad">Cantidad:</label>
+      <input type="number" id="quantity" name="quantity" value="1" min="1">
+      <label for="price">Precio:</label>
+      <input type="text" id="price" name="price" value="99.99" readonly>
+      <button type="submit">Agregar al carrito</button>
+    </form>
+  </div>
+</div>
+<div class="comments">
+  <h3>Comentarios</h3>
+  <ul>
+   //Aquí se incluirán los comentarios
+  </ul>
+</div>
+<?php
+	include("includes/comun/pie.php");
+	?>
 </html>
