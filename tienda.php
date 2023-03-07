063a94b7105a7f9dfbe35e0141152755cd281a16
alexpareja <aparej01@ucm.es>
2023-03-07 01:14:23 +0100

    +Pagina de tienda

--- tienda.php
+++ tienda.php
@@ -19,12 +19,33 @@
 	?>
 		<div id="productos">
 			<ul class = "productos">
-				<li>Producto 1</li>
-				<li>Producto 2</li>
-				<li>Producto 3</li>
-				<li>Producto 4</li>
-				<li>Producto 5</li>
-				<li>Producto 6</li>
+				<li>
+					<a href="producto.php"><img class="imgProducto" src="img/producto_id.png" alt="Imagen de Producto" /></br>Producto 1</a>
+				</li>
+				<li>
+					<a href="producto.php"><img class="imgProducto" src="img/producto_id.png" alt="Imagen de Producto" /></br>Producto 2</a>
+				</li>
+				<li>
+					<a href="producto.php"><img class="imgProducto" src="img/producto_id.png" alt="Imagen de Producto" /></br>Producto 3</a>
+				</li>
+				<li>
+					<a href="producto.php"><img class="imgProducto" src="img/producto_id.png" alt="Imagen de Producto" /></br>Producto 4</a>
+				</li>
+				<li>
+					<a href="producto.php"><img class="imgProducto" src="img/producto_id.png" alt="Imagen de Producto" /></br>Producto 5</a>
+				</li>
+				<li>
+					<a href="producto.php"><img class="imgProducto" src="img/producto_id.png" alt="Imagen de Producto" /></br>Producto 6</a>
+				</li>
+				<li>
+					<a href="producto.php"><img class="imgProducto" src="img/producto_id.png" alt="Imagen de Producto" /></br>Producto 7</a>
+				</li>
+				<li>
+					<a href="producto.php"><img class="imgProducto" src="img/producto_id.png" alt="Imagen de Producto" /></br>Producto 8</a>
+				</li>
+				<li>
+					<a href="producto.php"><img class="imgProducto" src="img/producto_id.png" alt="Imagen de Producto" /></br>Producto 9</a>
+				</li>
 			</ul>		
 		</div>
 	</div>
