4170c4164c778006949d9053f26099dafb5790b3
Cristina Morillo <cmoril01@ucm.es>
2023-03-11 17:15:32 +0100

    Contacto CSS

--- contacto.php
+++ contacto.php
@@ -9,14 +9,19 @@
   <?php
     include("includes/comun/cabecera.php");
   ?>
-  <div id="contact-form">
+  <div id="contacto-form">
     <form action="mailto:laquintacaja@gmail.com" method="post">
       <fieldset>
-      <legend> <h2> Contacto </h2></legend>
+      <legend>Contacto</legend>
+      <div>
       <p><label for="name">Nombre:</label>
       <input type="text" id="name" name="name" required></p>
+      </div>
+      <div>
       <p><label for="email">Email:</label>
       <input type="email" id="email" name="email" required></p>
+      </div>
+      <div>
       <p><label>Motivo de la consulta:</label></p>  
       <p><input type="radio" id="evaluacion" name="motivo" value="Evaluación">
       <label for="evaluacion" class="izq">Evaluación</label></p>
@@ -24,11 +29,18 @@
       <label for="sugerencias" class="izq">Sugerencias</label></p>
       <p><input type="radio" id="criticas" name="motivo" value="Críticas">
       <label for="criticas" class="izq">Críticas</label></p>
+      </div>
+      <div>
       <p><label for="message">Mensaje:</label></p>
       <p><textarea id="message" name="message" required></textarea></p>
+      </div>
+      <div>
       <p><input type="checkbox" id="terminos" name="terminos" required>
-      <label for="terminos" class="izq">Marque esta casilla para verificar que ha leído nuestros <a href="terminosycondiciones.php">términos y condiciones</a> del servicio</label></p>
-      <p><input type="submit" value="Enviar"></p>
+      <label for="terminos" class="terminos">Marque esta casilla para verificar que ha leído nuestros <a href="terminosycondiciones.php">términos y condiciones</a> del servicio</label></p>
+      </div>
+      <div>
+      <button type="submit">Enviar</button>
+      </div>
     </fieldset>  
     </form>
   </div>
