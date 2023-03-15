b5176645605f865ddcc39b507a86441e2c972998
alexpareja <aparej01@ucm.es>
2023-03-15 19:20:13 +0100

    plantillaTienda y tienda hecha para la plantilla

--- tienda.php
+++ tienda.php
@@ -1,63 +1,56 @@
 <?php
 require_once __DIR__.'/includes/Producto.php';
 
-?>
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-    <title>La Quinta Caja</title>
-    <link href="css/tienda.css" rel="stylesheet" type="text/css">
-    <meta charset="UTF-8">
-  </head>
-  <body>
-	<?php
-		require("includes/comun/cabecera.php");
-	?>
-	
-	<div id="tienda">
-	<?php
-		require("includes/comun/menuIzqTienda.php");
-		
-	?>
-	<h2>Productos</h2>
-	<ul class="botones">
-		<li>
-			<form action="compras.php">
-				<button type="submit">Compras</button>
-			</form>
-		</li>
-		<li>
-			<form action="anadirProd.php">
-				<button type="submit"> Añadir Producto</button>
-			</form>
-		</li>
-	</ul>
 
-		<div id="productos">
-			<ul class = "productos">
-				<?php
-					$productos = Producto::getTienda();
-					$i = 1;
-					foreach($productos as $prod){
-						//$prod = Producto::buscaPorId($i);
-						echo
-						'<li>
-						<a href="mostrarProducto.php?id='.$prod->getId().'">
-						<img class="imgProducto" src="img/producto_'.$prod->getId().'.png" alt="Imagen de Producto '.$prod->getId().'" />
-						</br>'.
-						$prod->getNombre().'
-						</a>
-						</li>';
-						$i++;
-					}
-				?>
+
+	$tituloPagina = 'Tienda';
+	$contenidoPrincipal = '';
+	$contenidoPrincipal .= <<<EOS
+		<div id="tienda">
+	EOS;
+	if (!isset($_SESSION["admin"])) {
+	$contenidoPrincipal .= <<<EOS
+			<ul class="botones">
+				<li>
+					<form action="compras.php">
+						<button type="submit">Compras</button>
+					</form>
+				</li>
+				<li>
+					<form action="anadirProd.php">
+						<button type="submit"> Añadir Producto</button>
+					</form>
+				</li>
 			</ul>
-			
-		</div>
-	</div>
+	EOS;
+	}
+	$contenidoPrincipal .= <<<EOS
+		<h2>Productos</h2>
+			<div id="productos">
+				<ul class = "productos">
+	EOS;
+$productos = Producto::getTienda();
+		foreach($productos as $prod){
+			$link = 'mostrarProducto.php?id='.$prod->getId();
+			$src = 'img/producto_'.$prod->getId().'.png';
+			$alt = 'Imagen de Producto '.$prod->getId();
+			$nombre = $prod->getNombre();
 	
-	<?php
-		include("includes/comun/pie.php");
-	?>
-  </body>
-</html>
+			$contenidoPrincipal .= <<<EOS
+					<li>
+						<a href=$link>
+						<img class="imgProducto" src=$src alt=$alt />
+						</br>
+						$nombre
+						</a>
+					</li>
+			EOS;
+		}
+		$contenidoPrincipal .= <<<EOS
+				</ul>		
+				</div>
+			</div>
+		EOS;
+
+require __DIR__.'/includes/plantillas/plantillaTienda.php';
+	
