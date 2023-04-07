e743d43b5590aff54174906e122165d792e7d654
luceroabregu <100693472+luceroabregu@users.noreply.github.com>
2023-04-07 03:38:03 +0200

    cabecera cambiada en plantilla.css

--- includes/comun/cabecera.php
+++ includes/comun/cabecera.php
@@ -1,25 +1,38 @@
-<div id="cabecera">
-	
-	<div class="contenedor">
-		<a href="tienda.php" class="tienda">Tienda </a>
-		<a href="blog.php" class="blog">Blog </a>
-		<a href="sobrenosotros.php" class="nosotros">Nosotros </a>
-		<img class="logo" src="img/logo.png" alt="Imagen Logo">
-		<a href="index.php"><h1 class="titulo">LA QUINTA CAJA</h1></a>
-		<a href="faqs.php"><img class="ayuda" src="img/ayuda.jpg" alt="Imagen FAQs"></a>
-		<a href="carrito.php"><img class="carrito" src="img/carrito.jpg" alt="Imagen Carrito"></a>
-		
+<header id="cabecera">
+	<div class="contenedor-flex">
+		<div class="oculto">
+			<input type="checkbox" name="" id="menu-icono"/>
+			<label for="menu-icono">
+				<img src="img/menu.png" class="menu" alt="Imagen Menu">
+			</label>
+			<nav class="paginas">
+				<ul>
+					<li><a href="tienda.php">Tienda</a></li>
+					<li><a href="blog.php">Blog</a></li>
+					<li><a href="sobrenosotros.php">Nosotros</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="principal">
+			<a href="index.php"><img src="img/logo.png" class="logo" alt="Imagen Logo"></a>
+			<a href="index.php" class="letras">LA QUINTA CAJA</a>
+		</div>
+		<div class="iconos">
+			<ul>
+				<li><a href="faqs.php"><img src="img/ayuda.png" class="ayuda" alt="Imagen ayuda"></a></li>
+				<li><a href="carrito.php"><img src="img/carrito.png" class="carrito" alt="Imagen carrito"></a></li>
 
-	<?php
-		if(!isset($_SESSION["login"])){
-			echo "<a href='login.php'><img class='iniciar' src='img/login.jpg' alt='Imagen Iniciar'></a>";
-		}	
-		else{
-			echo "<a href='miCuenta.php'><img class='cuenta' src='img/cuenta.jpg' alt='Imagen Cuenta'></a>";
-			echo "<a href='logout.php'><img class='iniciar' src='img/logout.jpg' alt='Imagen Cerrar'></a>";
-		}
-	?>
-
+				<?php
+					if(!isset($_SESSION["login"])){
+						echo "<li><a href='login.php'><img src='img/login.png' class='iniciar' alt='Imagen Iniciar'></a></li>";
+					}
+					else{
+						echo "<li><a href='miCuenta.php'><img src='img/cuenta.png' class='cuenta' alt='Imagen Cuenta'></a></li>";
+						echo "<li><a href='logout.php'><img src='img/logout.png' class='iniciar' alt='Imagen Cerrar'></a></li>";
+					}
+				?>
+			</ul>
+		</div>
 	</div>
-		
-</div>
+</header>
+
