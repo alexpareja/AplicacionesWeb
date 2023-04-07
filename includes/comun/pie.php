88470fde25d13ad33195a1c687357de438ca66a8
luceroabregu <100693472+luceroabregu@users.noreply.github.com>
2023-04-07 12:59:53 +0200

    pie responsive

--- includes/comun/pie.php
+++ includes/comun/pie.php
@@ -1,14 +1,32 @@
-<div id="pie">
-		<ul class='horizontalpie'>
-			<li><a href="terminosycondiciones.php" class="referencia">Términos y Condiciones</a></li>
-			<li><a href="https://www.instagram.com/laquintacaja/" class="referencia">@laquintacaja</a></li>
+<footer id="pie">
+	<div class="grupo1">
+		<div class="boxLogo">
+			<a href="index.php">
+				<img src="img/pie.png" class="logopie" alt="Logo de Pie">
+			</a>
+		</div>
+		<ul class="boxPaginas">
+			<li><a href="bocetos.php">BOCETOS</a></li>
+			<li><a href="miembros.php">CONOCENOS</a></li>
+			<li><a href="propuesta.php">PROPUESTA</a></li>
+			<li><a href="detalles.php">DETALLES</a></li>
+			<li><a href="contacto.php">CONTACTANOS</a></li>
+			<li><a href="planificacion.php">PLANIFICACION</a></li>
+			<li><a href="terminosycondiciones.php">TERMINOS Y CONDICIONES</a></li>
 		</ul>
-		<ul class='horizontalpie'>
-			<li><a href="propuesta.php" class="referencia">Propuesta</a></li>
-				<li><a href="detalles.php" class="referencia">Detalles</a></li>
-				<li><a href="bocetos.php" class="referencia">Bocetos</a></li>
-				<li><a href="miembros.php" class="referencia">Miembros</a></li>
-				<li><a href="planificacion.php" class="referencia">Planificación</a></li>
-				<li><a href="contacto.php" class="referencia">Contacto</a></li>
-		</ul>
-</div>
+		<div class="boxRedes">
+			<h2>SIGUENOS</h2>
+			<div class="redSocial">
+				<a href="https://www.instagram.com/laquintacaja/">
+					<img src="img/instagram.png" class="instagram" alt="Instagram">
+				</a>
+				<a href="https://twitter.com/LaQuintaCaja/">
+					<img src="img/twitter.png" class="twitter" alt="Twitter">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="grupo2">
+		<small>&copy; 2023 <b>La Quinta Caja</b> - Todos los Derechos Reservados.</small>
+	</div>
+</footer>
