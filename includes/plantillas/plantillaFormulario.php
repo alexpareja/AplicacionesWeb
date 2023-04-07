b665dc344f3ba2adb39fd36bca83a9837ee97ea4
luceroabregu <100693472+luceroabregu@users.noreply.github.com>
2023-04-07 14:32:37 +0200

    css pie fallos

--- includes/plantillas/plantillaFormulario.php
+++ includes/plantillas/plantillaFormulario.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html  lang="es">
 <head>
-		<meta charset="UTF-8">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $tituloPagina ?></title>
     <link rel="stylesheet" type="text/css" href="css/formulario.css"> 
     <script type="text/javascript" src="js/codigo.js"></script>
