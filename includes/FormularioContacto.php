876ff6f393ef93dddf521a4cd8881ffc52669e5b
Cristina Morillo <cmoril01@ucm.es>
2023-04-08 18:46:20 +0200

    CSS contacto

--- includes/FormularioContacto.php
+++ includes/FormularioContacto.php
@@ -11,39 +11,40 @@ class FormularioContacto extends Formulario
     {
         $html = <<<EOF
         <div id="contacto-form">
-            <fieldset>
-                <legend>Contacto</legend>
-                <div>
-                    <p><label for="name">Nombre:</label>
-                    <input type="text" id="name" name="name" required></p>
-                </div>
-                <div>
-                    <p><label for="email">Correo electrónico:</label>
-                    <input type="email" id="email" name="email" required></p>
-                </div>
-                <div>
-                    <p><label>Motivo de la consulta:</label></p>  
-                    <p><input type="radio" id="evaluacion" name="motivo" value="Evaluación">
-                    <label for="evaluacion" class="izq">Evaluación</label></p>
-                    <p><input type="radio" id="sugerencias" name="motivo" value="Sugerencias">
-                    <label for="sugerencias" class="izq">Sugerencias</label></p>
-                    <p><input type="radio" id="criticas" name="motivo" value="Críticas">
-                    <label for="criticas" class="izq">Críticas</label></p>
-                </div>
-                <div>
-                    <p><label for="message">Mensaje:</label></p>
-                    <p><textarea id="message" name="message" required></textarea></p>
-                </div>
-                <div>
-                    <p><input type="checkbox" id="terminos" name="terminos" required>
-                    <label for="terminos" class="terminos">Marque esta casilla para verificar que ha leído nuestros <a href="terminosycondiciones.php">términos y condiciones</a> del servicio</label></p>
-                </div>
-                <div>
-                    <button type="submit">Enviar</button>
-                </div>
-            </fieldset>
+        <fieldset>
+            <legend>Contacto</legend>
+            <div>
+                <p><label for="name">Nombre:</label>
+                <input type="text" id="name" name="name" required></p>
+            </div>
+            <div>
+                <p><label for="email">Correo electrónico:</label>
+                <input type="email" id="email" name="email" required></p>
+            </div>
+            <div>
+                <p><label>Motivo de la consulta:</label></p>  
+                <p><input type="radio" id="evaluacion" name="motivo" value="Evaluación">
+                <label for="evaluacion" class="izq">Evaluación</label></p>
+                <p><input type="radio" id="sugerencias" name="motivo" value="Sugerencias">
+                <label for="sugerencias" class="izq">Sugerencias</label></p>
+                <p><input type="radio" id="criticas" name="motivo" value="Críticas">
+                <label for="criticas" class="izq">Críticas</label></p>
+            </div>
+            <div>
+                <p><label for="message">Mensaje:</label></p>
+                <p><textarea id="message" name="message" required></textarea></p>
+            </div>
+            <div>
+                <p><input type="checkbox" id="terminos" name="terminos" required>
+                <label for="terminos" class="terminos">Marque esta casilla para verificar que ha leído nuestros <a href="terminosycondiciones.php">términos y condiciones</a> del servicio</label></p>
+            </div>
+            <div>
+                <button type="submit">Enviar</button>
+            </div>
+        </fieldset>
         </div>
         EOF;
+
         return $html;
     }
 }
