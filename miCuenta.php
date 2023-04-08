533b75aa638637af71d08951642f59dc7b16bff0
Cristina Morillo <cmoril01@ucm.es>
2023-04-08 11:51:19 +0200

    CSS

--- miCuenta.php
+++ miCuenta.php
@@ -16,42 +16,55 @@ $contenidoPrincipal = <<<EOS
 <ul class="listaMiCuenta">
   <li>
     <a href="listadeseos.php">
-      <div class="imagen">
         <div class="imagen-contenedor">
             <img class='imgListaDeseos' src="img/listadeseos.jpg" alt="Imagen Favoritos">
         </div>
-      </div>
+        <p>Favoritos</p>
     </a>
   </li>
   <li>
     <a href="miPerfil.php">
-      <div class="imagen">
         <div class="imagen-contenedor">
             <img class='imgMiPerfil' src="img/miPerfil.jpg" alt="Imagen Mi perfil">
-        </div>>
-      </div>
+        </div>
+        <p>Mi Perfil</p>
     </a>
   </li>
   <li>
     <a href="misCompras.php">
-      <div class="imagen">
         <div class="imagen-contenedor">
             <img class='imgMisCompras' src="img/misCompras.png" alt="Imagen Mis compras">
         </div>
-      </div>
+        <p>Mis Compras</p>
     </a>
   </li>
 </ul>
 
-<div id="contacto">
-    <p>¿Tienes algún problema?</p>
-    <h2>Contacta con nosotros</h2>
+<ul class="listaMiCuenta">
+  <li>
     <a href="contacto.php">
-    <div class="imagen-contacto">
-        <img class='imgContacto' src="img/contacto.png" alt="Imagen Contacto">
-    </div>
+        <div class="imagen-contenedor">
+            <img class='imgContacto' src="img/contacto.png" alt="Imagen Contacto">
+        </div>
+        <p>Contacto</p>
     </a>
-</div>
+  </li>
+  <li>
+    <a href="miPerfil.php">
+        <div class="imagen-contenedor">
+            <img class='imgMiPerfil' src="img/miPerfil.jpg" alt="Imagen Mi perfil">
+        </div>
+    </a>
+  </li>
+  <li>
+    <a href="logout.php">
+        <div class="imagen-contenedor">
+            <img class='imgLogout' src="img/logoutMiCuenta.jpg" alt="Imagen Logout">
+        </div>
+        <p>Cerrar Sesión</p>
+    </a>
+  </li>
+</ul>
 EOS;
 
 require __DIR__.'/includes/plantillas/plantilla.php';
