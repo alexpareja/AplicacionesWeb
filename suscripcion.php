7ef4e91666280aad96adf5fe78368d103816639d
Cristina Morillo <cmoril01@ucm.es>
2023-04-10 21:06:07 +0200

    suscipcion.php (no implementado)
    es para la siguiente entrega

---
+++ suscripcion.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__.'/includes/configuracion.php';
+$tituloPagina = 'Suscripción';
+$contenidoPrincipal = <<<EOS
+<div id="cabecera-perfil">
+  <h2>Zona Suscripción</h2>
+  <br>
+  <p>Aquí puedes encontrar toda la información sobre tu suscripción:</p>
+</div>
+EOS;
+
+require __DIR__.'/includes/plantillas/plantilla.php';
+?>
