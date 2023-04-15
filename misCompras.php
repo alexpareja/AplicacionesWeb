2db7e6a2d1bd6e90aa2b8192b0cc0aa0756f6e97
Cristina Morillo <cmoril01@ucm.es>
2023-04-15 16:48:35 +0200

    css tablas compras

--- misCompras.php
+++ misCompras.php
@@ -3,11 +3,11 @@ require_once __DIR__.'/includes/configuracion.php';
 
 
 	$tituloPagina = 'Mis Compras';
+	$nombre = $_SESSION['nombre'];
 	//Se muestran la tabla con las compras realizadas por el usuario hasta el momento de la base de datos de compras
 	$contenidoPrincipal = '';
 	$contenidoPrincipal .= <<<EOS
-	<div id="tienda">
-		<h2>Compras</h2>
+		<div id="tienda" class="tienda">
 		<ul class='botones'>
 		<li>
 			<button id="boton-filtros" type="submit" onclick="mostrarMenu()">Mostrar filtros</button>
@@ -19,6 +19,8 @@ require_once __DIR__.'/includes/configuracion.php';
 		</li>
 		
 		</ul>
+		<h2 class="titulo1">Mis compras</h2>
+		<h2 class="titulo2">$nombre</h2>
 	EOS;
 	$usuario = $_SESSION['id'];
 	$compras = es\ucm\fdi\aw\Compra::getAllComprasUsuario($usuario);
