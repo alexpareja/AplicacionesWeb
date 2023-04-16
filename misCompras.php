2582072e0192a60566ecd2400e727f69b54c1fe1
Cristina Morillo <cmoril01@ucm.es>
2023-04-17 00:20:15 +0200

    Cambiar boton filtros misCompras

--- misCompras.php
+++ misCompras.php
@@ -7,18 +7,13 @@ require_once __DIR__.'/includes/configuracion.php';
 	//Se muestran la tabla con las compras realizadas por el usuario hasta el momento de la base de datos de compras
 	$contenidoPrincipal = '';
 	$contenidoPrincipal .= <<<EOS
-		<div id="tienda" class="tienda">
-		<ul class='botones'>
-		<li>
-			<button id="boton-filtros" type="submit" onclick="mostrarMenu()">Mostrar filtros</button>
-		</li>
-		<li>
+	<div id="tienda" class="tienda">
+		<button id="boton-filtros" type="submit" onclick="mostrarMenu()" class="boton-filtros"><i class="fas fa-sliders-h white"></i></button>
+
 		<form action="tienda.php">
 			<button type="submit">Volver a la tienda</button>
 		</form>
-		</li>
 		
-		</ul>
 		<h2 class="titulo1">Mis compras</h2>
 		<h2 class="titulo2">$nombre</h2>
 	EOS;
