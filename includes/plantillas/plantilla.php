43da4979727ea9a3cab3e2780dea566bcb958ee7
sergioati <sergioat@ucm.es>
2023-05-02 18:52:16 +0200

    Arreglado error del merge

--- includes/plantillas/plantilla.php
+++ includes/plantillas/plantilla.php
@@ -7,14 +7,9 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="css/plantilla.css">
 	<link rel="shortcut icon" href="img/logo.ico" type="image/x-icon">
-<<<<<<< HEAD
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<script type="text/javascript" src="js/codigo.js"></script>
-=======
-	<script src="js/codigo.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
->>>>>>> cdab39b288c2e6b3425088cbe0a47854afe449cf
 	<title><?= $tituloPagina ?></title>
 </head>
 <body>
