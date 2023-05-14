7c2f826546e3b02a3d50ce790f5a66b116ac7c38
mbasca <100694317+mbasca@users.noreply.github.com>
2023-05-15 00:40:50 +0200

    Update 04-productos.sql

--- mysql/04-productos.sql
+++ mysql/04-productos.sql
@@ -1,7 +1,7 @@
 INSERT INTO `productos` (`id`, `nombre`, `descripcion`, `precio`, `oferta`, `premium`, `stockXS`, `stockS`, `stockM`, `stockL`, `stockXL`) VALUES
 (1, 'Caja Básica', 'Esta caja contiene una serie de productos básicos. Ideal para comenzar tu experiencia en La Quinta Caja.', 9.99, 0, 0, 14, 11, 14, 0, 9),
-(2, 'Caja de Halloween', 'Esta caja contiene ropa escalofriante para asustar a todos en Halloween', 39.99, 0, 0, 4, 2, 0, 0, 0),
-(3, 'Caja de Carnaval', 'Celebra el Carnaval con esta caja tan animada', 49.99, 1, 0, 11, 0, 12, 0, 10),
+(2, 'Caja de Halloween', 'Esta caja contiene ropa escalofriante para asustar a todos en Halloween', 39.99, 70, 0, 4, 2, 0, 0, 0),
+(3, 'Caja de Carnaval', 'Celebra el Carnaval con esta caja tan animada', 49.99, 20, 0, 11, 0, 12, 0, 10),
 (4, 'Caja Grande', 'En esta caja podrás comprar aún más prendas a un precio económico.', 19.99, 0, 0, 50, 54, 25, 26, 14),
 (5, 'Caja ExtraGrande', 'En esta caja, de nivel superior, encontrarás una gran cantidad de artículos de calidad', 59.99, 0, 0, 34, 13, 14, 13, 1),
 (6, 'Caja Premium', 'Esta caja es exclusiva para usuarios premium', 19.99, 0, 1, 20, 32, 48, 50, 21),
